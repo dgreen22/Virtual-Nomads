e7176a667157833d7b92a4661ff9563302c2233a
David Green <David.e.green@gmail.com>
2016-11-12 16:07:10 -0500

    favicon and fix email

--- app/mailers/admin_notifier_mailer.rb
+++ app/mailers/admin_notifier_mailer.rb
@@ -1,7 +1,7 @@
 class AdminNotifierMailer < ApplicationMailer
 	
-  default :from => 'jessica@virtualnomads.com'
-  
+  default :from => 'Booking Request <jessica@virtualnomads.com>'
+
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_prospect_notifier(prospect)
     @prospect = prospect
