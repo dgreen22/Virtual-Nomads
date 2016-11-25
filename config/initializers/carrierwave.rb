3395095e4fb26bbc304d83c45691c97bd0947d0b
David Green <David.e.green@gmail.com>
2016-11-25 00:12:54 -0500

    image storage added

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,19 @@
-CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                        # required
-  config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     'xxx',                        # required
-    aws_secret_access_key: 'yyy',                        # required
-    region:                'eu-west-1',                  # optional, defaults to 'us-east-1'
-    host:                  's3.example.com',             # optional, defaults to nil
-    endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
-  }
-  config.fog_directory  = 'name_of_directory'                          # required
-  config.fog_public     = false                                        # optional, defaults to true
-  config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
+if Rails.env.development? || Rails.env.test?
+  CarrierWave.configure do |config|
+    config.storage = :file
+  end
+end
+
+# Use AWS storage if in production
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider:              'AWS',                        # required
+      aws_access_key_id:     ENV['S3_ACCESS_KEY'],                        # required
+      aws_secret_access_key: ENV['S3_SECRET_KEY'],
+      region:                ENV['S3_REGION'],                  # optional, defaults to 'us-east-1'                        # required
+    }
+    config.fog_directory  = ENV['S3_BUCKET']                          # required
+  end
 end
